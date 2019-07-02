4d16b21b1db43dd38a2186f5659f3510888e53cf
Gabriel Braga <kevencript@gmail.com>
2019-07-02 15:32:30 -0300

    Criando Projeto em React

---
+++ prime-codes/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Ola</button>
+    </div>
+  );
+}
+
+export default App;
