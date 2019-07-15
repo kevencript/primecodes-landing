89930625bd15c9f157b6783242927a6644b38c44
Gabriel Braga <kevencript@gmail.com>
2019-07-15 19:31:06 -0300

    Finalizando as rotas

--- server/src/ controllers/contatoController.js
+++ server/src/ controllers/contatoController.js
@@ -16,7 +16,8 @@ exports.inserir_contato = async (req, res) => {
     // inserindo nova mensagem no banco
     const mensagem = new contatoMessage(mensagemPronta);
     await mensagem.save();
-    res.status(200).send(mensagem);
+    res.send(mensagem);
+    console.log("Mensagem enviada com sucesso");
 
     // lidando com erros
   } catch (error) {
@@ -26,11 +27,28 @@ exports.inserir_contato = async (req, res) => {
 };
 
 // Listar todas as mensagens de contato
-exports.listar_todos_contatos = (req, res) => {
-  res.send("- CÓDIGO NÃO IMPLEMENTADO");
+exports.listar_todos_contatos = async (req, res) => {
+  try {
+    // buscando todas as mensagens no db
+    const messages = await contatoMessage.find();
+    res.json(messages);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json("Server Error");
+  }
 };
 
 // Listar uma mensagem de contato através do ID
-exports.listar_por_id = (req, res) => {
-  res.send("- CÓDIGO NÃO IMPLEMENTADO");
+exports.listar_por_id = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const message = await contatoMessage.findOne({
+      _id: id
+    });
+
+    res.json(message);
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).json("Server Error");
+  }
 };
